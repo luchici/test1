e5da0dd7df27cf718bc0f960f7e1dfe26dcafd56
DESKTOP-F1ROUTL\root1 <luchici123@gmail.com>
2022-08-30 22:22:25 +0300

    aaa

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+  public static void main(String[] args) {
+    String here = "dddd";
+//        sdasdasdasdasdasd
+    String dddd = "aaaaa";
+    String s2 = "aaaaa";
+    String s3 = "aaaaa";
+    String S5 = here;
+    int a = 7;
+    int b = 7;
+    double b1 = 7;
+    double b2 = 7;
+    Integer c1 = 5;
+    Integer c3 = 5;
+    Integer c4 = 5;
+    Integer c2 = 5;
+    boolean b11 = false;
+    if (b11) {
+      System.out.println();
+    }
+  }
+
+  public static int add() {
+    return 1;
+  }
+}
